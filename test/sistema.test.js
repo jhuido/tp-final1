8dcbead0644968d17f932a020c50678ca535906c
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-07 20:58:31 -0300

    'TDD'

--- test/sistema.test.js
+++ test/sistema.test.js
@@ -4,3 +4,9 @@ test("Obtener primer proceso",()=>{
     var proceso=new Proceso();
     expect(proceso.proceso[0]).toBe("Cola de salida");
 })
+
+test("Agregar ubicaciones del proceso",()=>{
+    var proceso=new Proceso(["Cola de salida","Centro de facturación","centro de calidad","centro de distribución"]);
+    expect(proceso.proceso[0]).toBe("Cola de salida");
+    expect(proceso.proceso[1]).toBe("Centro de facturación");
+})
