4853c4a235a492ade39ce1405dee7c9913087d3c
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-07 21:02:57 -0300

    'TDD'

--- src/Proceso.js
+++ src/Proceso.js
@@ -1,6 +1,8 @@
-function Proceso(){
+function Proceso(ubicaciones){
     this.proceso=new Array();
-    this.proceso[0]="Cola de salida";
+    for(i=0;i<ubicaciones.length;i++){
+        this.proceso[i]=ubicaciones[i];
+    }
 }
 
 module.exports=Proceso;
--- test/sistema.test.js
+++ test/sistema.test.js
@@ -1,7 +1,7 @@
 const Proceso=require('../src/Proceso.js');
 
 test("Obtener primer proceso",()=>{
-    var proceso=new Proceso();
+    var proceso=new Proceso(["Cola de salida"]);
     expect(proceso.proceso[0]).toBe("Cola de salida");
 })
 
